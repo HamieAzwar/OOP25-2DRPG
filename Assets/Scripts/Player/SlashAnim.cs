f03720f4c4662f011891d207ef04cc90ea76572d
HamieAzwar <hamieazwar003@gmail.com>
2025-06-21 11:29:06 +0800

    First Iteration

---
+++ Assets/Scripts/Player/SlashAnim.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SlashAnim : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    private void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (ps && !ps.IsAlive())
+        {
+            DestroySelf();
+        }
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
